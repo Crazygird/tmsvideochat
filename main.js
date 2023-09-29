6e45925436302f9e9a0c0fd20d069af429d6fb3e
Mide <95974074+Crazygird@users.noreply.github.com>
2023-09-29 15:17:40 -0700

    dhfa

---
+++ main.js
@@ -0,0 +1,5 @@
+const APP_ID = "094f89e4e4fd4405a450acd2f9864215"
+const TOKEN = "007eJxTYNiz4eAyrXP/21WinQ9227rxpDaWr9getuRNRNcR869mJQYKDAaWJmkWlqkmqSZpKSYmBqaJJqYGickpRmmWFmYmRoamG3TEUxsCGRkC/LczMzJAIIjPwpCbmJnHwAAAZ7ce6Q=="
+const CHANNEL = "main"
+
+const client = AgoraRTC.createClient({mode: 'rtc', codec: 'vp8'})
