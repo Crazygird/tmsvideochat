9216329cf6674b5ff617e8fec82f7640bfd7f64b
Mide <95974074+Crazygird@users.noreply.github.com>
2023-10-02 09:10:29 -0700

    potato

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const APP_ID = "094f89e4e4fd4405a450acd2f9864215"
-const TOKEN = "007eJxTYNiz4eAyrXP/21WinQ9227rxpDaWr9getuRNRNcR869mJQYKDAaWJmkWlqkmqSZpKSYmBqaJJqYGickpRmmWFmYmRoamG3TEUxsCGRkC/LczMzJAIIjPwpCbmJnHwAAAZ7ce6Q=="
+const TOKEN = "007eJxTYOAyf7khK7BtXdnjXewGT7/Odbq9wVx1WfyjghRGsQ2XlO8qMBhYmqRZWKaapJqkpZiYGJgmmpgaJCanGKVZWpiZGBmaur2WSm0IZGTo+3KXhZEBAkF8FobcxMw8BgYAbf0gFg=="
 const CHANNEL = "main"
 
 const client = AgoraRTC.createClient({mode: 'rtc', codec: 'vp8'})
